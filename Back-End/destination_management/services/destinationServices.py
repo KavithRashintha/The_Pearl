78a598d0c49cb58681e6313e87f8b9c64609dba9
KavithRashintha <wgkrashintha@gmail.com>
2025-02-18 20:42:27 +0530

    Destination Management Service Modified

--- Back-End/destination_management/services/destinationServices.py
+++ Back-End/destination_management/services/destinationServices.py
@@ -1,10 +1,11 @@
 from models import destinationModels
 from sqlalchemy.orm import Session
 from schemas import destinationSchemas
+from models.destinationModels import Destinations
 
 
 def create_destination(db: Session, data: destinationSchemas.DestinationCreated):
-    new_destination = destinationModels(**data.model_dump())
+    new_destination = Destinations(**data.model_dump())
     db.add(new_destination)
     db.commit()
     db.refresh(new_destination)
@@ -12,15 +13,15 @@ def create_destination(db: Session, data: destinationSchemas.DestinationCreated)
 
 
 def get_all_destinations(db: Session):
-    return db.query(destinationModels).all()
+    return db.query(Destinations).all()
 
 
 def get_destination(db: Session, destination_id: int):
-    return db.query(destinationModels).filter(destinationModels.id == destination_id).first()
+    return db.query(Destinations).filter(Destinations.id == destination_id).first()
 
 
 def update_destination(db: Session, destination: destinationSchemas.DestinationCreated, destination_id: int):
-    retrieved_destination = db.query(destinationModels).filter(destinationModels.id == destination_id).first()
+    retrieved_destination = db.query(Destinations).filter(Destinations.id == destination_id).first()
     if retrieved_destination:
         for key, value in destination.model_dump().items():
             setattr(retrieved_destination, key, value)
@@ -30,7 +31,7 @@ def update_destination(db: Session, destination: destinationSchemas.DestinationC
 
 
 def delete_destination(db: Session, destination_id: int):
-    retrieved_destination = db.query(destinationModels).filter(destinationModels.id == destination_id).first()
+    retrieved_destination = db.query(Destinations).filter(Destinations.id == destination_id).first()
     if retrieved_destination:
         db.delete(retrieved_destination)
         db.commit()
