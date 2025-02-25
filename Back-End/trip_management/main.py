8d1e86c5fcb049a1332730d755ae4d57a3794db4
KavithRashintha <wgkrashintha@gmail.com>
2025-02-25 23:06:51 +0530

    Create Trip Function Created

--- Back-End/trip_management/main.py
+++ Back-End/trip_management/main.py
@@ -1,13 +1,13 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends, HTTPException
+from schemas import tripSchemas
+from services import tripServices
+from db import get_db
+from sqlalchemy.orm import Session
+from typing import List
 
 app = FastAPI()
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
-
-
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
+@app.post("/trips/add", response_model=tripSchemas.Trip)
+def create_trip(trip: tripSchemas.TripCreated, db: Session = Depends(get_db)):
+    return tripServices.create_trip(db, trip)
