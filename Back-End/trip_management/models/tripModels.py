4f897dbc036b6f294d4964546f8f8adedc5fba0e
KavithRashintha <wgkrashintha@gmail.com>
2025-02-25 22:59:45 +0530

    Trip Model Created

--- Back-End/trip_management/models/tripModels.py
+++ Back-End/trip_management/models/tripModels.py
@@ -1,1 +1,19 @@
+from db import Base
+from sqlalchemy import Integer, Column, String, Float
+from sqlalchemy.dialects.postgresql import ARRAY
+
+
+class Trip(Base):
+    __tablename__ = "trips"
+
+    id = Column(Integer, primary_key=True, index=True)
+    touristId = Column(Integer)
+    tourGuideId = Column(Integer)
+    destinations = Column(ARRAY(String))
+    numberOfAdults = Column(Integer)
+    numberOfChildren = Column(Integer)
+    numberOfDays = Column(Integer)
+    tripStatus = Column(String)
+    tripPayment = Column(Float)
+    paymentStatus = Column(String)
 
