763d97eacc1e6b5da4553908ff551dbb0a6d040b
KavithRashintha <wgkrashintha@gmail.com>
2025-02-25 23:03:20 +0530

    Trip Schemas Created

---
+++ Back-End/trip_management/schemas/tripSchemas.py
@@ -0,0 +1,33 @@
+from pydantic import BaseModel
+from typing import List
+
+
+class TripBase(BaseModel):
+    touristId: int
+    tourGuideId: int
+    destinations: List[str]
+    numberOfAdults: int
+    numberOfChildren: int
+    numberOfDays: int
+    tripStatus: str
+    tripPayment: float
+    paymentStatus: str
+
+
+class TripCreated(TripBase):
+    pass
+
+
+class TripStatusUpdate(BaseModel):
+    tripStatus: str
+
+
+class TripPaymentStatusUpdate(BaseModel):
+    paymentStatus: str
+
+
+class Trip(TripBase):
+    id: int
+
+    class config:
+        from_attribute = True
