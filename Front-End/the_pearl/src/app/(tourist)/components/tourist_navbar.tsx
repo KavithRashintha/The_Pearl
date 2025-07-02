663aa88ccbbd2e1ad3932b730dcd2cf80f7ac89c
KavithRashintha <wgkrashintha@gmail.com>
2025-07-02 22:12:58 +0530

    Tourist navbar modified with the pearl logo

--- Front-End/the_pearl/src/app/(tourist)/components/tourist_navbar.tsx
+++ Front-End/the_pearl/src/app/(tourist)/components/tourist_navbar.tsx
@@ -2,6 +2,7 @@
 
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
+import Image from "next/image";
 
 export default function TouristNavBar() {
     const pathname = usePathname();
@@ -15,10 +16,16 @@ export default function TouristNavBar() {
     ];
 
     return (
-        <nav className="bg-purple-100 py-4">
-            <div className="container mx-auto flex justify-between items-center pl-6 pr-12">
-                <Link href="/" className="text-lg font-bold text-blue-600">
-                    The Pearl
+        <nav className="bg-purple-100 py-2">
+            <div className="container mx-auto flex justify-between items-center pl-0 pr-12">
+                <Link href="/" className="relative w-28 h-10">
+                    <Image
+                        src="/images/the_pearl_logo.png"
+                        alt="The Pearl Logo"
+                        fill
+                        className="object-contain"
+                        quality={100}
+                    />
                 </Link>
                 <div className="flex space-x-14 text-sm">
                     {navItems.map((item) => (
